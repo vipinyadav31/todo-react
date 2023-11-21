c465b76200927da2dd7ff236950b159d960552e5
vipin yadav <vipinyadav31088@gmail.com>
2023-11-21 12:01:21 +0530

    ui updeted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 
-// import ServerData from './component/Hooks/ServerData';
-// import Crud from './component/CURD/Crud';
-// import Form from './component/FormDepth/Form';
-// import UncontrolForm from './component/FormDepth/UncontrolForm';
-// import NewForm from './component/FormDepth/NewForm';
 import Todo from './component/Todos/Todo';
 function App() {
   return (
-    <>
-    {/* <Form /> */}
-    {/* <ServerData /> */}
-    {/* <Crud /> */}
-    {/* <UncontrolForm /> */}
-    {/* <NewForm /> */}
     <Todo />
-    </>
   );
 }
 
